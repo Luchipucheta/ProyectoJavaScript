1d3061bb5dfaf267d226660e21be77547dc9c0c9
Luciana <puchetaluchi@gmail.com>
2026-08-25 21:42:50 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// un texto
+console.log("Prueba del enrutado")
+
+
+//alert
+alert("Bienvenidos a mi página web")
+
+//prompt 
+
+
+let cursos = prompt("Que cursos hiciste?")
+console.log("Cursos: " + cursos)
+
+let curso1 = parseInt(prompt("Cuantos meses duró el primer curso?"))
+console.log("Duró: " + curso1)
+
+let curso2 = parseInt(prompt("Cuantos meses duró el segundo curso?"))
+console.log("Duró: " + curso2)
+
+let resultado = curso1 + curso2
+console.log(resultado)
